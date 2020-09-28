fcf761843782c91fed5689dc8f9785621d317888
Hanyuchen <exclison@foxmail.com>
2020-09-28 17:20:59 +0800

    提交

--- webhooks.js
+++ webhooks.js
@@ -1,16 +1,16 @@
 var http = require('http')
 var createHandler = require('github-webhook-handler')
-var handler = createHandler({ path: '/webhooks', secret: 'myHashSecret' })
+var handler = createHandler({ path: '/docker_ci', secret: '123456' })
 // 上面的 secret 保持和 GitHub 后台设置的一致
 
-function run_cmd(cmd, args, callback) {
-    var spawn = require('child_process').spawn;
-    var child = spawn(cmd, args);
-    var resp = "";
+// function run_cmd(cmd, args, callback) {
+//     var spawn = require('child_process').spawn;
+//     var child = spawn(cmd, args);
+//     var resp = "";
 
-    child.stdout.on('data', function (buffer) { resp += buffer.toString(); });
-    child.stdout.on('end', function () { callback(resp) });
-}
+//     child.stdout.on('data', function (buffer) { resp += buffer.toString(); });
+//     child.stdout.on('end', function () { callback(resp) });
+// }
 // debug用
 // run_cmd('sh', ['./deploy-dev.sh'], function(text){ console.log(text) });
 
@@ -34,17 +34,17 @@ handler.on('*', function (event) {
     //   run_cmd('sh', ['./deploy-dev.sh'], function(text){ console.log(text) });
 })
  
-handler.on('push', function (event) {
-    console.log('Received a push event for %s to %s',
-        event.payload.repository.name,
-        event.payload.ref);
-        // 分支判断
-        if(event.payload.ref === 'refs/heads/master'){
-            console.log('deploy master..')
-            run_cmd('sh', ['./deploy-dev.sh'], function(text){ console.log(text) });
+// handler.on('push', function (event) {
+//     console.log('Received a push event for %s to %s',
+//         event.payload.repository.name,
+//         event.payload.ref);
+//         // 分支判断
+//         if(event.payload.ref === 'refs/heads/master'){
+//             console.log('deploy master..')
+//             run_cmd('sh', ['./deploy-dev.sh'], function(text){ console.log(text) });
 
-        }
-})
+//         }
+// })
 
 
 // handler.on('issues', function (event) {
