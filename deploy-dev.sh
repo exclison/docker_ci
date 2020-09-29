ad1a4932a847151288c455d0d46c870a7376b040
Hanyuchen <exclison@foxmail.com>
2020-09-29 11:05:09 +0800

    webhook

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -1,14 +1,13 @@
-ls
 
-# echo Deploy Project
+echo Deploy Project
 # docker-compose up -d --force-recreate --build
 
 # 获取最新版代码
-# git pull
+git pull
 
 # 强制重新编译容器
-# docker-compose down
-# docker-compose up -d --force-recreate --build
+docker-compose down
+docker-compose up -d --force-recreate --build
 
 
 # 定制镜像
--- webhooks.js
+++ webhooks.js
@@ -29,23 +29,23 @@ handler.on('error', function (err) {
     console.error('Error:', err.message)
 })
 
-handler.on('*', function (event) {
-    console.log('on')
-    console.log('Received *');
-    run_cmd('sh', ['./deploy-dev.sh'], function(text){ console.log(text) });
-})
- 
-// handler.on('push', function (event) {
-//     console.log('Received a push event for %s to %s',
-//         event.payload.repository.name,
-//         event.payload.ref);
-//         // 分支判断
-//         if(event.payload.ref === 'refs/heads/master'){
-//             console.log('deploy master..')
-//             run_cmd('sh', ['./deploy-dev.sh'], function(text){ console.log(text) });
-
-//         }
+// handler.on('*', function (event) {
+//     console.log('on')
+//     console.log('Received *');
+//     run_cmd('sh', ['./deploy-dev.sh'], function(text){ console.log(text) });
 // })
+ 
+handler.on('push', function (event) {
+    console.log('Received a push event for %s to %s',
+        event.payload.repository.name,
+        event.payload.ref);
+        // 分支判断
+        if(event.payload.ref === 'refs/heads/master'){
+            console.log('deploy master..')
+            run_cmd('sh', ['./deploy-dev.sh'], function(text){ console.log(text) });
+
+        }
+})
 
 
 // handler.on('issues', function (event) {
